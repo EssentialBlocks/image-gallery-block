eaca716f64de7adf08ac1dbbd1021afe8d2171fb
Monir <fencemonir@gmail.com>
2023-03-22 11:27:39 +0600

    updated: controls

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'dd6ab4a3f21d16a734e9');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c9bfb11fbd2f28a60dc5');
